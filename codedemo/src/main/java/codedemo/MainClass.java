5d9d0da59a76a931c3b0c9afc92b8b37fd37edfc
neatla <neatla@DIN73003048.corp.capgemini.com>
2018-11-23 14:21:48 +0530

    first commit

---
+++ codedemo/src/main/java/codedemo/MainClass.java
@@ -0,0 +1,15 @@
+package codedemo;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hi! Welcome to capgemini");
+		greet();
+
+	}
+
+	public static void greet() {
+		System.out.println("inside greet");
+		
+	}
+}
